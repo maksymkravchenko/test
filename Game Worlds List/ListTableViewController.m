e8b53647e0b0b5f3bf8cf4bcb16e9ac173cc1a57
max <max-mac@MacBook-Pro-max.local>
2016-02-29 01:50:00 +0200

    Finished with presentation of worlds names list, and login logic

--- Game Worlds List/ListTableViewController.m
+++ Game Worlds List/ListTableViewController.m
@@ -8,6 +8,138 @@
 
 #import "ListTableViewController.h"
 
+static NSString * const kTableViewCellIdentifier = @"Cell";
+
+static NSString * const kDefaultEmailValue = @"ios.test@xyrality.com";
+static NSString * const kDefaultPasswordValue = @"password";
+
+//parsing fetched data from URL strings
+static NSString * const kOnlineStringValue = @"online";
+static NSString * const kAllAvailableWorldsKey = @"allAvailableWorlds";
+static NSString * const kWorldNameKey = @"name";
+static NSString * const kDescriptionKey = @"description";
+static NSString * const kWorldStatusKey = @"worldStatus";
+
+@interface ListTableViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property BOOL updatingWorldsList;
+@property (nonatomic) NSURLRequest *request;
+@property NSArray <NSString *> *availableWorldsNamesList;
+
+@end
+
 @implementation ListTableViewController
+
+- (void)viewDidAppear:(BOOL)animated
+{
+	[self updateWorldsList];
+	[super viewDidAppear:animated];
+}
+
+- (NSURLRequest *)request
+{
+	NSMutableString *mutableURLpath = [NSMutableString stringWithString:@"http://backend1.lordsandknights.com/XYRALITY/WebObjects/BKLoginServer.woa/wa/worlds?"];
+	
+	NSString *deviceType = [NSString stringWithFormat:@"deviceType=%@-%@%@", [[UIDevice currentDevice] model], [[UIDevice currentDevice] systemName], [[UIDevice currentDevice] systemVersion]];
+	NSString *deviceUUID = [NSString stringWithFormat:@"&deviceId=%@", [[NSUUID UUID] UUIDString]];
+	NSString *login = [NSString stringWithFormat:@"&login=%@", kDefaultEmailValue];
+	NSString *password = [NSString stringWithFormat:@"&password=%@", kDefaultPasswordValue];
+	
+	[mutableURLpath appendString:deviceType];
+	[mutableURLpath appendString:deviceUUID];
+	[mutableURLpath appendString:login];
+	[mutableURLpath appendString:password];
+	
+	NSString *normalizedURLPath = [mutableURLpath stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+	
+	NSURL *requestURL = [NSURL URLWithString:normalizedURLPath];
+	
+	NSMutableURLRequest *result = [NSMutableURLRequest requestWithURL:requestURL];
+	result.HTTPMethod = @"POST";
+	
+	return result;
+}
+
+- (void)updateWorldsList
+{
+	__weak ListTableViewController *weakSelf = self;
+	NSURLSession *session = [NSURLSession sharedSession];
+	
+	[[session dataTaskWithRequest:self.request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+		if (error == nil)
+		{
+			NSDictionary *result = [NSPropertyListSerialization propertyListWithData:data options:0 format:nil error:&error];
+			NSArray *allWorldsList = [result objectForKey:kAllAvailableWorldsKey];
+
+			[weakSelf storeAvailableWorldsList:allWorldsList];
+			dispatch_async(dispatch_get_main_queue(), ^{
+				[weakSelf.tableView reloadData];
+			});
+		}
+		else
+		{
+			dispatch_async(dispatch_get_main_queue(), ^{
+				[self handleURLError];
+			});
+		}
+	}] resume];
+}
+
+- (void)storeAvailableWorldsList:(NSArray *)worldsList
+{
+	NSMutableArray *result = [NSMutableArray new];
+	NSDictionary *worldStatus = nil;
+	NSString *description = nil;
+	for (NSDictionary *currentWorld in worldsList)
+	{
+		worldStatus = [currentWorld objectForKey:kWorldStatusKey];
+		description = [worldStatus objectForKey:kDescriptionKey];
+		if ([description isEqualToString:kOnlineStringValue])
+		{
+			[result addObject:[currentWorld objectForKey:kWorldNameKey]];
+		}
+	}
+
+	self.availableWorldsNamesList = result;
+}
+
+- (void)handleURLError
+{
+	UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"An Error occurred" message:@"Please Check Internet Connection." preferredStyle:UIAlertControllerStyleAlert];
+	
+	UIAlertAction *onRetryAction = [UIAlertAction actionWithTitle:@"Try Again" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+		[self updateWorldsList];
+		[alert dismissViewControllerAnimated:YES completion:nil];
+	}];
+	
+	UIAlertAction *onCancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+		[alert dismissViewControllerAnimated:YES completion:nil];
+	}];
+	
+	[alert addAction:onRetryAction];
+	[alert addAction:onCancelAction];
+	
+	[self presentViewController:alert animated:YES completion:nil];
+}
+
+#pragma mark - UITableView
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;
+{
+	return self.availableWorldsNamesList.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kTableViewCellIdentifier];
+ 
+	if (cell == nil)
+	{
+		cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:kTableViewCellIdentifier];
+	}
+
+	cell.textLabel.text = self.availableWorldsNamesList[indexPath.row];
+	return cell;
+}
 
 @end
--- Game Worlds List/ViewController.m
+++ Game Worlds List/ViewController.m
@@ -8,9 +8,7 @@
 
 #import "ViewController.h"
 
-static NSString * const kDefaultEmailValue = @"ios.test@xyrality.com";
-static NSString * const kDefaultPasswordValue = @"password";
-
+static NSString * const kShowWorldsSegueID = @"ShowWorldList";
 
 @interface ViewController () <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *emailTextField;
@@ -32,36 +30,103 @@ static NSString * const kDefaultPasswordValue = @"password";
 	[super viewWillDisappear:animated];
 }
 
-- (void)viewDidLoad {
-	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
-}
-
-//- (BOOL)validateEmailAddress
-//{
-//	NSString *emailRegEx = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,10}";
-//	NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegEx];
-
-//	if ([emailTest evaluateWithObject:email.text] == NO) {
-//		
-//		UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Test!" message:@"Please Enter Valid Email Address." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
-//		[alert show];
-//		[alert release];
-//		
-//		return;
-//	}
-//}
-
-
-
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+- (BOOL)validateEmailAddress:(NSString *)emailString
 {
+	BOOL result = NO;
+	if (emailString != nil)
+	{
+		NSString *emailRegEx = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,10}";
+		NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegEx];
+		
+		result = [emailTest evaluateWithObject:emailString];
+	}
+	return result;
+}
+
+- (IBAction)onMainAction:(id)sender
+{
+	if ([self validateEmailAddress:self.emailTextField.text] && ![self.passwordTextField.text isEqualToString:@""])
+	{
+		[self performSegueWithIdentifier:kShowWorldsSegueID sender:self];
+	}
+	else if (![self validateEmailAddress:self.emailTextField.text])
+	{
+		[self shakeTextField:self.emailTextField];
+		[self.emailTextField becomeFirstResponder];
+	}
+	else
+	{
+		[self shakeTextField:self.passwordTextField];
+		[self.passwordTextField becomeFirstResponder];
+	}
+}
+
+- (void)shakeTextField:(UITextField *)textField
+{
+	const int kMaxShakes = 6;
+	const CGFloat kShakeDuration = 0.05;
+	const CGFloat kShakeTransform = 4;
+	CGFloat direction = 1;
 	
+	for (int i = 0; i <= kMaxShakes; i++)
+	{
+		[UIView animateWithDuration:kShakeDuration delay:kShakeDuration * i options:UIViewAnimationOptionCurveEaseIn animations:^{
+			 if (i >= kMaxShakes)
+			 {
+				 textField.transform = CGAffineTransformIdentity;
+			 } else
+			 {
+				 textField.transform = CGAffineTransformMakeTranslation(kShakeTransform * direction, 0);
+			 }
+		 } completion:nil];
+		
+		direction *= -1;
+	}
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+	BOOL result = NO;
+	if (textField == self.emailTextField)
+	{
+		if ([self validateEmailAddress:self.emailTextField.text])
+		{
+			[self.emailTextField resignFirstResponder];
+			[self.passwordTextField becomeFirstResponder];
+			result = YES;
+		}
+		else
+		{
+			[self shakeTextField:self.emailTextField];
+		}
+	}
+	else
+	{
+		if (![self.passwordTextField.text isEqualToString:@""])
+		{
+			if ([self validateEmailAddress:self.emailTextField.text])
+			{
+				[self.passwordTextField resignFirstResponder];
+				[self performSegueWithIdentifier:kShowWorldsSegueID sender:self];
+			}
+			else
+			{
+				[self.passwordTextField resignFirstResponder];
+				[self.emailTextField becomeFirstResponder];
+				[self shakeTextField:self.emailTextField];
+			}
+
+			result = YES;
+		}
+		else
+		{
+			[self shakeTextField:self.passwordTextField];
+		}
+	}
+	
+	return result;
 }
 
 @end
